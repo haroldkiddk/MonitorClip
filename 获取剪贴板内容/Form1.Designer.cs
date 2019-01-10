28276157c99b94e469caac09a68811e7ee9820a3
haroldkiddk <haroldkidd@hotmail.com>
2019-01-10 09:47:12 +0800

    Monitor Clipboard

---
+++ 获取剪贴板内容/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace 获取剪贴板内容
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(504, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 168);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(504, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 270);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(504, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 获取剪贴板内容/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 获取剪贴板内容
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        protected static extern int SetClipboardViewer(int hWndViewer);
+        [DllImport("user32.dll")]
+        protected static extern bool ChangeClipboardChain(IntPtr hWndResume, IntPtr hWndNext);
+        [DllImport("user32.dll")]
+        protected static extern int SendMessage(IntPtr hWnd, int nMsg, IntPtr wParam, IntPtr lParam);
+        IntPtr hNextClipboardViewer;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hNextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
+        }
+
+        void ShowNotify()
+        {
+            dataGridView1.DataSource = null;
+            richTextBox1.Clear();
+            string Str = Clipboard.GetText();
+            if (Str.Contains("\t"))
+            {
+                DataTable dt = new DataTable();
+                string[] StrSplitEnter = Regex.Split(Str, "\r\n");
+                foreach(string s1 in StrSplitEnter)
+                {
+                    if (s1 != "")
+                    {
+                        DataRow row = dt.NewRow();
+                        dt.Rows.Add(row);
+                        string[] StrSplitTab = Regex.Split(s1, "\t");
+                        int cnt = 0;
+                        foreach (string s2 in StrSplitTab)
+                        {
+                            cnt++;
+                            if (cnt > dt.Columns.Count)
+                            {
+                                dt.Columns.Add(cnt.ToString());
+                            }
+                            dt.Rows[dt.Rows.Count - 1][cnt - 1] = s2;
+                        }
+                    }
+                }
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                richTextBox1.AppendText(Str);
+            }
+        }
+
+        void ShowImage()
+        {
+            pictureBox1.Image = Clipboard.GetImage();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_DRAWCLPBORAD = 0x308;
+            const int WM_CHANGCBCHAIN = 0X030D;
+            switch (m.Msg)
+
+            {
+                case WM_DRAWCLPBORAD:
+                    if (Clipboard.ContainsText())
+                    {
+                        ShowNotify();
+                    }
+                    else if (Clipboard.ContainsImage())
+                    {
+                        ShowImage();
+                    }
+                    SendMessage(hNextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                case WM_CHANGCBCHAIN:
+                    if (hNextClipboardViewer == m.WParam)
+                    {
+                        hNextClipboardViewer = m.LParam;
+                    }
+                    else
+                    {
+                        SendMessage(hNextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    }
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+    }
+}
